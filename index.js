d21afe89eb400298521fe4097e1985ee5ab2cdd7
kumar <abhishekshuklamath@gmail.com>
2020-09-09 11:41:14 -0700

    Adde js sliders

--- index.js
+++ index.js
@@ -5,137 +5,19 @@ $('#add').on('click', function() {
   var md = 0.5;
   var td;
   var zd;
-  var v0 = document.getElementById("v0").value;
-  var v1 = document.getElementById("v1").value;
-  var v2 = document.getElementById("v2").value;
-  var wc = document.getElementById("wc").value;
-  var ad = document.getElementById("ad").value;
-  var r = document.getElementById("r").value;
-  var p = document.getElementById("p").value;
+  var v0 = parseFloat(vslider.noUiSlider.get()[0]);
+  var v1 = parseFloat(vslider.noUiSlider.get()[1]);
+  var v2 = parseFloat(vslider.noUiSlider.get()[2]);
+  var wc = parseFloat(wslider.noUiSlider.get()[0]);
+  var wd = parseFloat(wslider.noUiSlider.get()[1]);
+  var r = parseFloat(pslider.noUiSlider.get()[0]);
+  var p = parseFloat(pslider.noUiSlider.get()[1]);
   var a = document.getElementById("a").value;
   var b = document.getElementById("b").value;
   var message1 = document.getElementById("message1");
   var message2 = document.getElementById("message2");
-  var warning = document.getElementById('warning');
-  var warning1 = document.getElementById('warning1');
-  var warning2 = document.getElementById('warning2');
-  var warning3 = document.getElementById('warning3');
-  var warning4 = document.getElementById('warning4');
-  var warning5 = document.getElementById('warning5');
-  var wd = Number(wc) + Number(ad);
-  if (v0 > 1) {
-    warning.textContent = "Answer should lie in between 0 and 1";
-    message1.textContent = "";
-    message2.textContent = "";
-    return;
-  } else {
-    warning.textContent = "";
-  }
-  if (v0 < 0) {
-    warning.textContent = "Answer should lie in between 0 and 1";
-    message1.textContent = "";
-    message2.textContent = "";
-    return;
-  } else {
-    warning.textContent = "";
-  }
-  if (v1 > 1) {
-    warning1.textContent = "Answer should lie in between 0 and 1";
-    message1.textContent = "";
-    message2.textContent = "";
-    return;
-  } else {
-    warning1.textContent = "";
-  }
-  if (v1 < 0) {
-    warning1.textContent = "Answer should lie in between 0 and 1";
-    message1.textContent = "";
-    message2.textContent = "";
-    return;
-  } else {
-    warning1.textContent = "";
-  }
-  if (v2 > 1) {
-    warning2.textContent = "Answer should lie in between 0 and 1";
-    message1.textContent = "";
-    message2.textContent = "";
-    return;
-  } else {
-    warning2.textContent = "";
-  }
-  if (v2 < 0) {
-    warning2.textContent = "Answer should lie in between 0 and 1";
-    message1.textContent = "";
-    message2.textContent = "";
-    return;
-  } else {
-    warning2.textContent = "";
-  }
-  if (v1 < v0) {
-    warning1.textContent = "V1 must be greater than V0";
-    message1.textContent = "";
-    message2.textContent = "";
-    return;
-  } else {
-    warning.textContent = "";
-  }
-  if (v2 < v1) {
-    warning2.textContent = "V2 must be greater than V1";
-    message1.textContent = "";
-    message2.textContent = "";
-    return;
-  } else {
-    warning2.textContent = "";
-  }
-  if (r < 0) {
-    warning3.textContent = "answer must lie in between 0 and 1 ";
-    message1.textContent = "";
-    message2.textContent = "";
-    return;
-  } else {
-    warning3.textContent = "";
-  }
-  if (p < 0) {
-    warning3.textContent = "answer must lie in between 0 and 1 ";
-    message1.textContent = "";
-    message2.textContent = "";
-    return;
-  } else {
-    warning3.textContent = "";
-  }
-  if (p > 1) {
-    warning3.textContent = "answer must lie in between 0 and 1 ";
-    message1.textContent = "";
-    message2.textContent = "";
-    return;
-  } else {
-    warning3.textContent = "";
-  }
-  if (r > p) {
-    warning3.textContent = "answer must lie in between 0 and p ";
-    message1.textContent = "";
-    message2.textContent = "";
-    return;
-  } else {
-    warning3.textContent = "";
-  }
-  if (a < 0) {
-    warning4.textContent = "Answer should be positive";
-    message1.textContent = "";
-    message2.textContent = "";
-    return;
-  } else {
-    warning4.textContent = "";
-  }
-  if (b < 0) {
-    warning5.textContent = "Answer should be positive";
-    message1.textContent = "";
-    message2.textContent = "";
-    return;
-  } else {
-    warning5.textContent = "";
+ 
 
-  }
 
   for (var i = 0; i < 5000; i++) {
     var sc = a * r * fd * (wd - wc) * v0 / ( a * (1 - md) + md);
@@ -152,23 +34,20 @@ $('#add').on('click', function() {
     md = Math.round(mdd.valueOf() * 1000000) / 1000000;
 
 
-    if (md > 0.999999) {
-      md = 1;
-    } else if (md < 0.000001) {
-      md = 0;
-    }
-
-    if (fd > 0.999999) {
-      fd = 1;
-    } else if (fd < 0.000001) {
-      fd = 0;
-    }
-    if (i === 4999) {
-
-      message1.textContent = md;
-      message2.textContent = fd;
-      break;
-    }
-
+    
+    
   }
+  if (md > 0.9999) {
+    md = 1;
+  } else if (md < 0.000001) {
+    md = 0;
+  }
+
+  if (fd > 0.9999) {
+    fd = 1;
+  } else if (fd < 0.000001) {
+    fd = 0;
+  }
+  message1.textContent = md;
+  message2.textContent = fd;
 });
